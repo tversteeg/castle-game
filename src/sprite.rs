8e21eac2c76e4fba8994e5b91157903c00f0e242
Thomas Versteeg <thomasversteeg@gmx.com>
2018-01-09 22:16:00 +0100

    Convert PNG images to BLIT format on build

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,19 +1,12 @@
-extern crate image;
-
 use specs::{Component, VecStorage};
 use geom::Position;
 use blit::*;
 
-const MASK_COLOR: u32 = 0xFFFF00FF;
-
 pub struct Sprite(BlitBuffer);
 
 impl Sprite {
-    pub fn new(file: &'static str) -> Self {
-        let dynamic_img = image::open(file).unwrap();
-        let img = dynamic_img.as_rgb8().unwrap();
-
-        Sprite(img.as_blit_buffer(MASK_COLOR))
+    pub fn new(buf: BlitBuffer) -> Self {
+        Sprite(buf)
     }
 
     pub fn draw(&self, pos: &Position, buffer: &mut Vec<u32>, buffer_size: (usize, usize)) {
