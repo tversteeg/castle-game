270648ef977f400cc5437ba098c7a79c51b4c0ca
Thomas Versteeg <thomasversteeg@gmx.com>
2018-02-16 23:58:27 +0100

    Add another archer

--- src/level.rs
+++ src/level.rs
@@ -68,10 +68,10 @@ pub fn place_turrets(world: &mut World, level: u8) {
                 .with(Destination(630.0))
                 .with(Health(20.0))
                 .with(Melee::new(5.0, 1.0))
-                //.with(Turret::new(1.0, 200.0, 5.0, 5.0))
-                //.with(Arrow(3.0))
+                .with(Turret::new(1.0, 200.0, 5.0, 5.0))
+                .with(Arrow(3.0))
+                .with(Line::new(0x000000))
                 //.with(Damage(5.0))
-                //.with(Line::new(0x000000))
                 .with(UnitState::Walk)
                 .build();
 
@@ -79,6 +79,18 @@ pub fn place_turrets(world: &mut World, level: u8) {
                 .with(Enemy)
                 .with(Sprite::new(enemy_soldier1))
                 .with(WorldPosition(Point::new(570.0, 200.0)))
+                .with(Walk::new(BoundingBox::new(Point::new(2.0, 5.0), Point::new(5.0, 10.0)), 10.0))
+                .with(BoundingBox::new(Point::new(0.0, 0.0), Point::new(10.0, 10.0)))
+                .with(Destination(10.0))
+                .with(Health(100.0))
+                .with(Melee::new(10.0, 1.0))
+                .with(UnitState::Walk)
+                .build();
+
+            world.create_entity()
+                .with(Enemy)
+                .with(Sprite::new(enemy_soldier1))
+                .with(WorldPosition(Point::new(370.0, 200.0)))
                 .with(Walk::new(BoundingBox::new(Point::new(2.0, 5.0), Point::new(5.0, 10.0)), 10.0))
                 .with(BoundingBox::new(Point::new(0.0, 0.0), Point::new(10.0, 10.0)))
                 .with(Destination(10.0))
